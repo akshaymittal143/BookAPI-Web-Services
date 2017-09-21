824955a5ae566606771ca26ec7f85cb52cd7212d
Akshay Mittal <akshaycanodia@gmail.com>
2017-09-20 20:34:43 -0500

    added body-parser for Verb:Put in Json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express= require('express'),
-    mongoose =require('mongoose');
+    mongoose =require('mongoose'),
+    bodyParser=require('body-parser');
 
 var db= mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -9,9 +10,19 @@ var app= express();
 
 var port= process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var bookRouter=express.Router();
 
 bookRouter.route('/Books')
+    .post(function (req,res) {
+        var book=new Book(req.body);
+
+        console.log(book);
+        res.send(book);
+
+    })
     .get(function (req, res) {
 
         var query={};
