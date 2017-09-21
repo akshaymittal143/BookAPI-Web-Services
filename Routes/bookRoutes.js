8e59444fb0d145892ee15e0b2fc23b33ba7c2ecf
Akshay Mittal <akshaycanodia@gmail.com>
2017-09-20 21:33:53 -0500

    updated the api using put method

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -35,6 +35,20 @@ var routes=function (Book) {
                 else
                     res.json(book);
             });
+        })
+        .put(function (req,res) {
+            Book.findById(req.params.bookId, function (err, book) {
+                if (err)
+                    res.status(500).send(err);
+                else
+                    book.title=req.body.title;
+                    book.author=req.body.author;
+                    book.genre=req.body.genre;
+                    book.read=req.body.read;
+                    book.save();
+                    res.json(book);
+            });
+
         });
     return bookRouter;
 };
