9df0595ab87d369f853ffc95c720d8b9b5a3b129
Akshay Mittal <akshaycanodia@gmail.com>
2017-09-21 21:51:13 -0500

    Added Self links using hateos

--- controller/bookController.js
+++ controller/bookController.js
@@ -20,13 +20,22 @@ var bookController=function (Book) {
         if(req.query.genre){
             query.genre=req.query.genre;
         }
-        //var responseJson={hello: "This is my API"};
+
         Book.find(query, function (err, books) {
             if(err)
                 res.status(500).send(err);
-            else
-                res.json(books);
-        });
+            else {
+                var returnBooks=[];
+                books.forEach(function (element, index, array)
+                {
+                    var newBook=element.toJSON();
+                    newBook.links={};
+                    newBook.links.self='http://'+ req.headers.host + '/api/books/' +newBook._id;
+                    returnBooks.push(newBook);
+                     });
+                res.json(returnBooks);
+            }
+            });
     };
 
     return {
